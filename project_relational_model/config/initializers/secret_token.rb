e8b34488168cd2e453e6c813dd235b9345c6cd05
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2013-03-26 10:57:11 +0100

    create rails project for relational model

---
+++ project_relational_model/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectRelationalModel::Application.config.secret_token = '26c1dd69b964dba62203c051560d3b5434b964ae770644bcbdf45d25443d6ae2885452b1afacfef5eee5c3764e66ab935e9094a82072ab6dc1c27bbb078aece6'
