a36e21c2de71dfef02d92efa2b4d2b8db3682960
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2013-04-04 00:09:32 +0200

    Meter class for benchmarks

---
+++ project_relational_model/app/models/meter.rb
@@ -0,0 +1,36 @@
+class Meter
+  def self.create_programmers(max)
+    time = Benchmark.realtime do
+      DataManager.create_programmers max
+    end
+    puts "Time elapsed #{time*1000} milliseconds"
+  end
+
+  def self.add_languages_to_programmer(max)
+    time = Benchmark.realtime do
+      DataManager.add_languages_to_programmers max
+    end
+    puts "Time elapsed #{time*1000} milliseconds"
+  end
+
+  def self.add_programmers_to_projects(max)
+    time = Benchmark.realtime do
+      DataManager.add_programmers_to_projects max
+    end
+    puts "Time elapsed #{time*1000} milliseconds"
+  end
+
+  def self.delete_programmers
+    time = Benchmark.realtime do
+      DataManager.delete_all_programmers
+    end
+    puts "Time elapsed #{time*1000} milliseconds"
+  end
+
+  def self.calculate_top10_languages
+    time = Benchmark.realtime do
+      #do something
+    end
+    puts "Time elapsed #{time*1000} milliseconds"
+  end
+end
