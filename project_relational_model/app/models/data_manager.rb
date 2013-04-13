088552359aeaa5e9bd9d2231a94ea60fdbe88a55
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2013-04-13 23:38:47 +0200

    fix bug

--- project_relational_model/app/models/data_manager.rb
+++ project_relational_model/app/models/data_manager.rb
@@ -60,8 +60,8 @@ class DataManager
   end
 
   def self.get_top10_languages
-    top10 = ProgrammingLanguage.all.sort(:limit => [0,10]).to_a.sort_by {|p|[-p.programmers.count, p.name]}
-    top10.each do |lang|
+    top10 = ProgrammingLanguage.all.to_a.sort_by {|p|[-p.programmers.count, p.name]}
+    top10.take(10).each do |lang|
       p lang.name+": "+lang.programmers.count.to_s+" Programmers"
     end
   end
